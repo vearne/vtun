9bc5e847c73b32ece95e3cd3ba8e01ed34fa206a
netbyte <alex.tsai@protonmail.com>
2021-09-27 15:51:11 +0800

    update script

--- scripts/net_optimize.sh
+++ scripts/net_optimize.sh
@@ -1,29 +1,22 @@
 #!bin/bash
 
 # set ulimit
-if grep -Eqi "ulimit -SHn" /etc/profile || grep -Eqi "* soft nofile|* hard nofile" /etc/security/limits.conf; then
-        echo "ulimit has been optimized!"
-else
-        echo "ulimit -SHn 1024000" >> /etc/profile
-        ulimit -n 1024000
-        echo "* soft nofile 1024000" >> /etc/security/limits.conf
-        echo "* hard nofile 1024000" >> /etc/security/limits.conf
-fi
+echo "set ulimit"
+echo "ulimit -SHn 1024000" >> /etc/profile
+ulimit -n 1024000
+echo "* soft nofile 1024000" >> /etc/security/limits.conf
+echo "* hard nofile 1024000" >> /etc/security/limits.conf
 
 # enable bbr
-if grep -Eqi "bbr" /etc/sysctl.conf; then
-        echo "bbr has enabled!"
-else
-        echo "enable bbr"
-        echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-        echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-        sysctl -p
-        sysctl net.ipv4.tcp_available_congestion_control
-fi
+echo "enable bbr"
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
 # enable ip forward
 echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl net.ipv4.tcp_available_congestion_control
 sysctl -p
+
 # show results
 echo "results:"
 ulimit -n
