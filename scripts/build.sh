7bb8f4b04ce15226ac612dc7c9069a888ed4c038
Alex <alex.tsai@protonmail.com>
2021-10-18 00:05:49 +0800

    support openwrt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,13 @@
 #!bin/bash
 export GO111MODULE=on
 
-#Linux
+#Linux amd64
 GOOS=linux GOARCH=amd64 go build -o ./bin/vtun-linux-amd64 ./main.go
-#Linux arm
+#Linux arm64
 GOOS=linux GOARCH=arm64 go build -o ./bin/vtun-linux-arm64 ./main.go
-#Mac OS
+#Mac amd64
 GOOS=darwin GOARCH=amd64 go build -o ./bin/vtun-darwin-amd64 ./main.go
+#Openwrt mipsel_24kc
+GOOS=linux GOARCH=mipsle GOMIPS=softfloat -o ./bin/vtun-mipsel_24kc ./main.go
 
 echo "DONE!!!"
