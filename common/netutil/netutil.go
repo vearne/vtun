8d6346e82ec787895864f6f4ebad20e27cb69ce6
netbyte <alex.tsai@protonmail.com>
2022-05-10 09:43:48 +0800

    fix code style

--- common/netutil/netutil.go
+++ common/netutil/netutil.go
@@ -100,11 +100,13 @@ func LookupIP(domain string) string {
 }
 
 func IsIPv4(packet []byte) bool {
-	return 4 == (packet[0] >> 4)
+	flag := packet[0] >> 4
+	return flag == 4
 }
 
 func IsIPv6(packet []byte) bool {
-	return 6 == (packet[0] >> 4)
+	flag := packet[0] >> 4
+	return flag == 6
 }
 
 func GetIPv4Source(packet []byte) net.IP {
