567efcd7dccb4538d9da085540645c9fbf2f56b1
netbyte <alex.tsai@protonmail.com>
2022-05-17 19:59:44 +0800

    add default cert

--- scripts/make_certs.sh
+++ scripts/make_certs.sh
@@ -4,5 +4,5 @@ domain="vtun.org"
 email="admin@vtun.org"
 
 echo "make cert"
-openssl req -new -nodes -x509 -out ./certs/client.pem -keyout ./certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=$domain/emailAddress=$email"
+openssl req -new -nodes -x509 -out ./certs/server.pem -keyout ./certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=$domain/emailAddress=$email"
 
