8e14e18c64cbd13a7ffec3e8df94b338810f9e2b
rfyiamcool <rfyiamcool@163.com>
2023-08-25 10:28:28 +0800

    fix: data race for counter
    Signed-off-by: rfyiamcool <rfyiamcool@163.com>

--- common/counter/counter.go
+++ common/counter/counter.go
@@ -18,19 +18,19 @@ func IncrReadBytes(n int) {
 	atomic.AddUint64(&_totalReadBytes, uint64(n))
 }
 
-//IncrWrittenBytes increments the number of bytes written
+// IncrWrittenBytes increments the number of bytes written
 func IncrWrittenBytes(n int) {
 	atomic.AddUint64(&_totalWrittenBytes, uint64(n))
 }
 
 // GetReadBytes returns the number of bytes read
 func GetReadBytes() uint64 {
-	return _totalReadBytes
+	return atomic.LoadUint64(&_totalReadBytes)
 }
 
 // GetWrittenBytes returns the number of bytes written
 func GetWrittenBytes() uint64 {
-	return _totalWrittenBytes
+	return atomic.LoadUint64(&_totalWrittenBytes)
 }
 
 // PrintBytes returns the bytes info
